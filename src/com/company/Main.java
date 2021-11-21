fcd330ca9e3c4b3bb2c660701f5e9e41be33588c
Tapz1 <chris.a.tapia@gmail.com>
2021-11-21 13:31:31 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ContactService contactService = new ContactService();
+        try {
+            contactService.addContact(
+                    "222",
+                    "Colonel",
+                    "Sanders",
+                    "6035554224",
+                    "226 Wallaby Way");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("All contacts:");
+        for(Map.Entry<String, List<String>> entry : contactService.getContactMap().entrySet()){
+            String key = entry.getKey();
+            List<String> values = entry.getValue();
+            System.out.println("Key: " + key);
+            System.out.println("Value: " + values);
+        }
+    }
+}
